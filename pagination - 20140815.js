7049a2ab0b90c96f38b46e2dda0b3c3dac5f9956
CodeMeow <codemeow@yahoo.com>
2015-09-07 11:41:47 +0800

    add something

---
+++ pagination - 20140815.js
@@ -0,0 +1,265 @@
+﻿(function ($) {
+    $.extend({
+        cpagination: function (cfg) {
+            //config
+            cfg.bx = !cfg.bx ? $('.cpg') : $('.' + cfg.bx);//pagination shell (default : cpg)
+
+            cfg.it = !cfg.it ? '<li />' : '<' + cfg.it + ' />';//pagination item (default : li)
+            cfg.ct = !cfg.ct ? 'cpg_it' : cfg.ct;//pagination item css name (default : cpg_it)
+            cfg.ht = !cfg.ht ? 'cpg_ht' : cfg.ht;//pagination active item css name (default : cpg_ht)
+            cfg.ft = !cfg.ft ? 'cpg_ft' : cfg.ft;//first button css name (default : cpg_ft)
+            cfg.lt = !cfg.lt ? 'cpg_lt' : cfg.lt;//last button css name (default : cpg_lt)
+            cfg.pv = !cfg.pv ? 'cpg_pv' : cfg.pv;//prev button css name (default : cpg_pv)
+            cfg.nt = !cfg.nt ? 'cpg_nt' : cfg.nt;//next button css name (default : cpg_nt)
+            cfg.hb = !cfg.hb ? 'cpg_hb' : cfg.hb;//fix button active css name (default : cpg_hb)
+            cfg.ftp = !cfg.ftp ? '<<' : cfg.ftp;//first button plain
+            cfg.ltp = !cfg.ltp ? '>>' : cfg.ltp;//last button plain
+            cfg.pvp = !cfg.pvp ? '<' : cfg.pvp;//prev button plain
+            cfg.ntp = !cfg.ntp ? '>' : cfg.ntp;//next button plain
+
+            cfg.tp = !cfg.tp ? {} : cfg.tp;//postback data template
+            cfg.tep = function () { }//data template class
+            cfg.tep.prototype = cfg.tp;
+
+            cfg.u = !cfg.u ? '' : cfg.u;//ajax url address (default : empty)
+            cfg.id = !cfg.id ? {} : cfg.id;//ajax init post data (default : empty)
+            cfg.fr = typeof (cfg.fr) == 'undefined' ? false : cfg.fr;//first page number (false : first page  true : last page)
+            cfg.sz = !cfg.sz ? 10 : cfg.sz;//page size (default : 10)
+            cfg.sh = !cfg.sh ? 5 : cfg.sh;//show page number (default : 5)
+            cfg.er = !cfg.er ? function () { } : cfg.er;//ajax error handle
+            cfg.hd = !cfg.hd ? function () { } : cfg.hd;//data handle
+            cfg.c = 0;//total data
+            cfg.n = 1;//total pages
+            cfg.i = 1;//current page
+            cfg.itm = [];
+
+            cfg.ftn = {};
+            cfg.ltn = {};
+            cfg.pvn = {};
+            cfg.ntn = {};
+
+            function hbs(cfg) {//setting fix button css style
+                if (cfg.i == 1) {
+                    cfg.ftn.removeClass(cfg.hb);
+                    cfg.pvn.removeClass(cfg.hb);
+                } else {
+                    cfg.ftn.addClass(cfg.hb);
+                    cfg.pvn.addClass(cfg.hb);
+                }
+                if (cfg.i == cfg.n) {
+                    cfg.ltn.removeClass(cfg.hb);
+                    cfg.ntn.removeClass(cfg.hb);
+                } else {
+                    cfg.ltn.addClass(cfg.hb);
+                    cfg.ntn.addClass(cfg.hb);
+                }
+            }
+
+            function add(bn, i, cfg) {//add click event to bn button, i is page number
+                bn.unbind();
+                bn.click(function () {
+                    //todo ajax post
+                    cfg.i = i;
+                    var dat = new cfg.tep();
+                    dat.i = i;
+                    $.ajax({
+                        type: "POST",
+                        url: cfg.u,
+                        data: dat,
+                        dataType: "json",
+                        success: function (dat) {
+                            if (!!dat.s) { cfg.sz = dat.s; }
+                            trim(dat.c, cfg);
+                            cfg.hd(dat.d);
+                        },
+                        error: cfg.er
+                    });
+                });
+            }
+            function rmv(bn) {//remove click event to bn button
+                bn.unbind();
+            }
+
+            function fix(bn, typ, cfg) {//add fix button click event typ : 1 first 2 prev 3 next 4 last
+                bn.unbind();
+                bn.click(function () {
+                    var dat = new cfg.tep();
+                    switch (typ) {
+                        case 1:
+                            if (cfg.i == 1) { return; }
+                            cfg.i = 1;
+                            dat.i = 1;
+                            break;
+                        case 2:
+                            if (cfg.i == 1) { return; }
+                            cfg.i = cfg.i - 1;
+                            dat.i = cfg.i;
+                            break;
+                        case 3:
+                            if (cfg.i == cfg.n) { return; }
+                            cfg.i = cfg.i + 1;
+                            dat.i = cfg.i;
+                            break;
+                        case 4:
+                            if (cfg.i == cfg.n) { return; }
+                            cfg.i = cfg.n;
+                            dat.i = cfg.i;
+                            break;
+                    }
+                    $.ajax({
+                        type: "POST",
+                        url: cfg.u,
+                        data: dat,
+                        dataType: "json",
+                        success: function (dat) {
+                            if (!!dat.s) { cfg.sz = dat.s; }
+                            trim(dat.c, cfg);
+                            cfg.hd(dat.d);
+                        },
+                        error: cfg.er
+                    });
+                });
+            }
+
+            function trim(co, cfg) {//adjust to the correct location
+                cfg.c = co;
+                cfg.n = Math.ceil(cfg.c / cfg.sz);
+                cfg.n = cfg.n > 0 ? cfg.n : 1;
+                var i;
+                var a = 1;
+                if (cfg.n <= cfg.sh) {
+                    for (; a <= cfg.n; a++) {
+                        var it_btn = i_fac(a - 1, a, cfg);
+                    }
+                } else {
+                    var off = Math.floor(cfg.sh / 2);
+                    if (cfg.i <= off) {
+                        for (; a <= cfg.sh; a++) {
+                            var it_btn = i_fac(a - 1, a, cfg);
+                        }
+                    } else if (cfg.n - cfg.i + 1 <= off) {
+                        for (a = cfg.sh; a > 0; a--) {
+                            var it_btn = i_fac(cfg.sh - a, cfg.n - a + 1, cfg);
+                        }
+                    } else {
+                        for (; a <= cfg.sh; a++) {
+                            var it_btn = i_fac(a - 1, cfg.i - off + a - 1, cfg);
+                        }
+                    }
+                }
+                i_rea(cfg);
+                hbs(cfg);
+            }
+
+            function i_fac(off, a, cfg) {//create page number button
+                if (!cfg.itm[off]) {
+                    cfg.itm[off] = $(cfg.it, { 'class': cfg.ct });
+                }
+                cfg.itm[off].text(a);//fill page number button plain
+                if (a == cfg.i) { cfg.itm[off].addClass(cfg.ht); rmv(cfg.itm[off]); }
+                else { cfg.itm[off].removeClass(cfg.ht); add(cfg.itm[off], a, cfg); }
+                return cfg.itm[off];
+            }
+
+            function i_rea(cfg) {//rearrange button
+                var n = 0;
+                cfg.ftn.detach();
+                cfg.pvn.detach();
+                cfg.ntn.detach();
+                cfg.ltn.detach();
+                for (n in cfg.itm) {
+                    cfg.itm[n].detach();
+                }
+                cfg.bx.append(cfg.ftn);
+                cfg.bx.append(cfg.pvn);
+                for (n in cfg.itm) {
+                    cfg.bx.append(cfg.itm[n]);
+                }
+                cfg.bx.append(cfg.ntn);
+                cfg.bx.append(cfg.ltn);
+            }
+
+            function init(co, cfg) {
+                cfg.c = co;
+                cfg.n = Math.ceil(cfg.c / cfg.sz);
+                cfg.n = cfg.n > 0 ? cfg.n : 1;
+                cfg.i = cfg.fr ? cfg.n : 1;
+                var a = 1;
+                if (cfg.n <= cfg.sh) {
+                    for (; a <= cfg.n; a++) {
+                        var it_btn = i_fac(a - 1, a, cfg);
+                    }
+                } else {
+                    if (!cfg.fr) {
+                        for (; a <= cfg.sh; a++) {
+                            var it_btn = i_fac(a - 1, a, cfg);
+                        }
+                    } else {
+                        for (a = cfg.sh; a > 0; a--) {
+                            var it_btn = i_fac(cfg.sh - a, cfg.n - a + 1, cfg);
+                        }
+                    }
+                }
+                cfg.ftn = $(cfg.it, { 'class': cfg.ft }).text(cfg.ftp);
+                fix(cfg.ftn, 1, cfg);
+                cfg.pvn = $(cfg.it, { 'class': cfg.pv }).text(cfg.pvp);
+                fix(cfg.pvn, 2, cfg);
+                cfg.ntn = $(cfg.it, { 'class': cfg.nt }).text(cfg.ntp);
+                fix(cfg.ntn, 3, cfg);
+                cfg.ltn = $(cfg.it, { 'class': cfg.lt }).text(cfg.ltp);
+                fix(cfg.ltn, 4, cfg);
+                i_rea(cfg);
+                hbs(cfg);
+            }
+
+            function ini(co, cfg) {
+                cfg.bx.empty();
+                cfg.c = 0;
+                cfg.n = 1;
+                cfg.i = 1;
+                cfg.itm = [];
+                cfg.ftn = {};
+                cfg.ltn = {};
+                cfg.pvn = {};
+                cfg.ntn = {};
+
+                init(co, cfg);
+            }
+
+            cfg.init = function (dat) {
+                (function (cfg) {
+                    $.ajax({
+                        type: "POST",
+                        url: cfg.u,
+                        data: dat,
+                        dataType: "json",
+                        success: function (dat) {
+                            if (!!dat.s) { cfg.sz = dat.s; }
+                            ini(dat.c, cfg);
+                            cfg.hd(dat.d);
+                        },
+                        error: cfg.er
+                    });
+                })(this);
+            };
+
+            //init call
+            (function (cfg) {
+                $.ajax({
+                    type: "POST",
+                    url: cfg.u,
+                    data: cfg.id,
+                    dataType: "json",
+                    success: function (dat) {
+                        if (!!dat.s) { cfg.sz = dat.s; }
+                        init(dat.c, cfg);
+                        cfg.hd(dat.d);
+                    },
+                    error: cfg.er
+                });
+            })(cfg);
+
+            return cfg;
+        }
+    });
+}(jQuery));
---
+++ tree - 20140815.js
@@ -0,0 +1,233 @@
+﻿(function ($) {
+    $.extend({
+        ctree: function (cfg) {
+            //config
+            cfg.bx = !cfg.bx ? $('.ctree') : $('.' + cfg.bx);//tree shell (default : ctree)
+
+            cfg.pn = !cfg.pn ? '<ul />' : '<' + cfg.pn + ' />';//tree parent node (default : ul)
+            cfg.sn = !cfg.sn ? '<li />' : '<' + cfg.sn + ' />';//tree sub node (default : li)
+            cfg.nn = !cfg.nn ? '<a />' : '<' + cfg.nn + ' />';//tree inner node (default : a)
+            cfg.cpn = !cfg.cpn ? 'cpn' : cfg.cpn;//parent node style name (default : cpn)
+            cfg.csn = !cfg.csn ? 'csn' : cfg.csn;//sub node style name (default : csn)
+            cfg.cin = !cfg.cin ? 'cin' : cfg.cin;//inner node style name (default : cin)
+            cfg.crt = !cfg.crt ? 'crt' : cfg.crt;//inner node style name (default : crt)
+
+            cfg.clf = !cfg.clf ? 'clf' : cfg.clf;//leaf node style name (default : clf)
+            cfg.lfn = !cfg.lfn ? 'lf' : cfg.lfn;//leaf node property name (default : lf)
+
+            cfg.chk = !cfg.chk ? 0 : cfg.chk;//checkbox: 0 no 1 leaf 2 all (default : 0)
+            cfg.cch = !cfg.cch ? 'cch' : cfg.cch;//checkbox style name (default : cch)
+            cfg.rel = typeof (cfg.rel) == 'undefined' ? false : cfg.rel;//whether or not checkbox relation
+
+
+            cfg.dpn = !cfg.dpn ? 'dpn' : cfg.dpn;//display property name (default : dpn)
+
+            cfg.tp = !cfg.tp ? {} : cfg.tp;//postback data template
+            cfg.tep = function () { }//data template class
+            cfg.tep.prototype = cfg.tp;
+
+            cfg.k = !cfg.k ? 'k' : cfg.k;//key property name (default : k)
+            cfg.pk = !cfg.pk ? 'pk' : cfg.pk;//parent key property name (default : pk)
+            cfg.id = !cfg.id ? {} : cfg.id;//ajax init post data (default : empty)
+            cfg.u = !cfg.u ? '' : cfg.u;//ajax url address (default : empty)
+            cfg.hd = !cfg.hd ? function () { } : cfg.hd;//ajax successful handle
+            cfg.er = !cfg.er ? function () { } : cfg.er;//ajax error handle
+
+            cfg.nd = [];//node list
+            cfg.nc = [];//node content list
+            cfg.ctr = [];//checkbox tree
+
+            cfg.ckh = !cfg.ckh ? function () { } : cfg.ckh;//checkbox changed event handle
+            cfg.all;//all the tree
+
+            cfg.frk;//fork result
+
+            cfg.fork = function (cd) {
+                var that = this;
+                if (typeof cd !== 'undefined') {
+                    this.all.hide();
+                    $.ajax({
+                        type: "POST",
+                        url: this.u,
+                        data: cd,
+                        dataType: "json",
+                        success: function (dat) {
+                            that.frk = rend(that, toJson(dat));
+                            that.hd(dat);
+                        },
+                        error: this.er
+                    });
+                    return;
+                }
+                if (typeof this.frk !== 'undefined') {
+                    this.frk.remove();
+                }
+                this.all.show();
+            }
+
+            cfg.set = function (b) {
+                (function f(l, b, cfg) {
+                    for (var j in l) {
+                        var d = l[j].p.attr('checked') == 'checked';
+                        if (d ^ b) {
+                            l[j].p.attr('checked', b);
+                            cfg.ckh(b, l[j].p, l[j].d);
+                        }
+                        f(l[j].l, b, cfg);
+                    }
+                })(this.ctr, b, this);
+            }
+
+
+            function toJson(str) {
+                if (typeof str === 'string' && str != '') {
+                    return (new Function("return " + str))();
+                }
+                return str;
+            }
+
+            function rend(cfg, dat, pk) {
+                var box = cfg.bx;
+                if (typeof pk !== 'undefined') {
+                    box = cfg.nd[pk];
+                }
+                var pn = $(cfg.pn, { 'pk': pk, 'class': cfg.cpn });
+                if (typeof pk === 'undefined' && typeof cfg.all === 'undefined') {
+                    cfg.all = pn;
+                }
+                for (var i in dat) {
+                    var k = '_' + dat[i][cfg.k];
+                    var lf = false;
+                    if (typeof dat[i][cfg.lfn] !== 'undefined') {
+                        lf = dat[i][cfg.lfn];
+                    }
+                    var sn = $(cfg.sn, { 'k': k, 'class': cfg.csn });
+                    if (typeof pk === 'undefined') {
+                        sn.addClass(cfg.crt);
+                    }
+                    if (lf) {
+                        sn.addClass(cfg.clf);
+                    }
+                    cfg.nd[k] = sn;
+                    if ((cfg.chk == 1 && lf) || cfg.chk == 2) {
+                        var chk = $('<input />',
+                            { 'k': k, 'type': 'checkbox', 'class': cfg.cch });
+                        if (cfg.rel && cfg.chk == 2) {
+                            var wrap = { p: chk, l: [], d: dat[i] };
+                            cfg.ctr[k] = wrap;
+                            if (typeof pk !== 'undefined' &&
+                                typeof cfg.ctr[pk] !== 'undefined') {
+                                var b = cfg.ctr[pk].p.attr('checked') == 'checked';
+                                chk.attr('checked', b);
+                                cfg.ckh(b, chk, dat[i]);
+                                cfg.ctr[pk].l[k] = wrap;
+                            }
+                            (function (k) {
+                                chk.change(function () {
+                                    var b = $(this).attr('checked') == 'checked';
+                                    cfg.ckh(b, cfg.ctr[k].p, cfg.ctr[k].d);
+                                    if (typeof cfg.ctr[k] !== 'undefined') {
+                                        var l = cfg.ctr[k].l;
+                                        (function f(l, b) {
+                                            for (var j in l) {
+                                                var d = l[j].p.attr('checked') == 'checked';
+                                                if (d ^ b) {
+                                                    l[j].p.attr('checked', b);
+                                                    cfg.ckh(b, l[j].p, l[j].d);
+                                                }
+                                                f(l[j].l, b);
+                                            }
+                                        })(l, b);
+                                    }
+                                });
+                            })(k);
+                        }
+                        sn.append(chk);
+                    }
+                    var n = $(cfg.nn, { 'k': k, 'class': cfg.cin });
+                    if (lf) {
+                        n.addClass(cfg.clf);
+                    }
+                    n.text(dat[i][cfg.dpn]);
+                    if (!lf) {
+                        (function (sn, k) {
+                            n.click(function () {
+                                if (typeof sn.sw === 'undefined' || sn.sw == 0) {
+                                    sn.sw = 1;
+                                    opn(cfg, k);
+                                    return;
+                                }
+                                if (typeof sn.sw !== 'undefined' && sn.sw == 2) {
+                                    sn.sw = 1;
+                                    cls(cfg, k);
+                                    return;
+                                }
+                            });
+                        })(sn, k);
+                    }
+                    sn.append(n);
+                    pn.append(sn);
+                }
+                cfg.nc[pk] = pn;
+                box.append(pn);
+                return pn;
+            }
+
+            function opn(cfg, pk) {
+                var nd = cfg.nd[pk];
+                if (typeof nd === 'undefined') {
+                    return;
+                }
+                var ctn = cfg.nc[pk];
+                if (typeof ctn !== 'undefined') {
+                    ctn.css({ 'display': 'block' });//todo
+                    nd.sw = 2;
+                    return;
+                }
+                var dat = new cfg.tep();
+                dat[cfg.pk] = pk.substr(1);
+                $.ajax({
+                    type: "POST",
+                    url: cfg.u,
+                    data: dat,
+                    dataType: "json",
+                    success: function (dat) {
+                        rend(cfg, toJson(dat), pk);
+                        nd.sw = 2;
+                        cfg.hd(dat);
+                    },
+                    error: cfg.er
+                });
+            }
+
+            function cls(cfg, pk) {
+                var nd = cfg.nd[pk];
+                if (typeof nd === 'undefined') {
+                    return;
+                }
+                var ctn = cfg.nc[pk];
+                if (typeof ctn !== 'undefined') {
+                    ctn.css({ 'display': 'none' });//todo
+                }
+                nd.sw = 0;
+            }
+
+            //init call
+            (function (cfg) {
+                $.ajax({
+                    type: "POST",
+                    url: cfg.u,
+                    data: cfg.id,
+                    dataType: "json",
+                    success: function (dat) {
+                        rend(cfg, toJson(dat));
+                        cfg.hd(dat);
+                    },
+                    error: cfg.er
+                });
+            })(cfg);
+
+            return cfg;
+        }
+    });
+}(jQuery));
